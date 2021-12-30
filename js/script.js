8d0742477b90b3011a9f10ce21502fd866607abb
suzanadeazevedo <suzanadeazevedo@hotmail.com>
2021-12-30 18:16:26 -0300

    Versão 1.0

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function calculate(){
+    const etanol =  document.getElementById("etanol").value
+    const gasolina = document.getElementById("gasolina").value
+    const resultado = etanol/gasolina
+    
+    var text=''
+    if(resultado >= 0.8){
+        text="Gasolina é a melhor opção"
+    } else {
+        if (resultado <= 0.6){
+        text="Etanol é a melhor opção"
+    } else {
+        text="Pode abastecer com etanol ou gasolina"
+    }
+    }
+    document.getElementById("text_area").innerText=text
+}
+
+function ClearFields(){
+    document.getElementById("etanol").value=""
+    document.getElementById("gasolina").value=""
+    document.getElementById("text_area").value=""
+}
